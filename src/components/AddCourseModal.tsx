e7cb8708b4ccfda3a8234cbf312236dc5245ab19
JoshuaPark <soromiso@gmail.com>
2022-04-14 21:24:47 +0900

    react update

--- src/components/AddCourseModal.tsx
+++ src/components/AddCourseModal.tsx
@@ -1,39 +1,65 @@
-import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonLabel, IonModal, IonRow, IonTitle, IonToolbar } from '@ionic/react'
-import React from 'react'
+import {
+  IonButton,
+  IonCol,
+  IonContent,
+  IonDatetime,
+  IonGrid,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonModal,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import React from "react";
 
 const AddCourseModal: React.FC<{
-    show: boolean;
-    onCancel: () => void;
+  show: boolean;
+  onCancel: () => void;
 }> = (props) => {
   return (
     <IonModal isOpen={props.show}>
-        <IonHeader>
-            <IonToolbar>
-                <IonTitle>Add Course</IonTitle>
-            </IonToolbar>
-        </IonHeader>
-        <IonContent>
-          <IonGrid>
-            <IonRow>
-              <IonCol>
-                <IonItem>
-                  <IonLabel position='floating'>Course Title</IonLabel>
-                  <IonInput type='text'></IonInput>
-                </IonItem>
-              </IonCol>
-            </IonRow>
-            <IonRow className='ion-text-center'>
-              <IonCol>
-                <IonButton fill='clear' color='dark' onClick={props.onCancel}>Cancel</IonButton>
-              </IonCol>
-              <IonCol>
-              <IonButton expand='block' color='secondary'>Save</IonButton>
-              </IonCol>
-            </IonRow>
-          </IonGrid>
-        </IonContent>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Add Course</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating">Course Title</IonLabel>
+                <IonInput type="text"></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel>Enroll Date</IonLabel>
+                <IonDatetime locale="ko-KR"/>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow className="ion-text-center">
+            <IonCol>
+              <IonButton fill="clear" color="dark" onClick={props.onCancel}>
+                Cancel
+              </IonButton>
+            </IonCol>
+            <IonCol>
+              <IonButton expand="block" color="secondary">
+                Save
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
     </IonModal>
-  )
-}
+  );
+};
 
-export default AddCourseModal
+export default AddCourseModal;
