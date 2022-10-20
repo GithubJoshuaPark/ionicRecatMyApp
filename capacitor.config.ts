8061e982f02cd2f271c54ccffdc7eeaaeabf90c6
JoshuaPark <32838405+GithubJoshuaPark@users.noreply.github.com>
2022-10-20 20:47:14 +0900

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.mobileCem.lguplus',
-  appName: 'mobileCemLgUplus',
+  appId: '',
+  appName: '',
   webDir: 'build',
   bundledWebRuntime: false
 };
